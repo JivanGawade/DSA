5ffb6c85ed3129e4e52fae5db4c997b065a56cb3
JivanGawade <jivangawade77@gmail.com>
2026-09-12 18:37:33 +0530

    Time: 71 ms (32.78%), Space: 134.4 MB (45.39%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -1,31 +1,19 @@
 class Solution {
     public ListNode removeNodes(ListNode head) {
-        if (head == null) return null;
-        head = reverseList(head);
-        
-        ListNode curr = head;
-        int maxSoFar = curr.val;
-        while (curr != null && curr.next != null) {
-            if (curr.next.val < maxSoFar) {
-                curr.next = curr.next.next;
-            } else {
-                maxSoFar = curr.next.val;
-                curr = curr.next;
-            }
-        }
-        return reverseList(head);
+     Stack<ListNode> st=new Stack<>();
+     ListNode temp=head;
+     while(temp!=null){
+        while(st.size()>0 && st.peek().val<temp.val)
+            st.pop();
+        st.push(temp);
+
+        temp=temp.next;
     }
-    private ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
-        
-        while (curr != null) {
-            ListNode nextTemp = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = nextTemp;
-        }
-        
-        return prev;
+    while(st.size()>0){
+        ListNode top=st.pop();
+        top.next=temp;
+        temp=top;
+    }
+    return temp;
     }
 }
